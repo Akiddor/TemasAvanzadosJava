15e68fc1c034f84272cba86b840ad5060aa2064d
Akiddor <Akiddor7@gmail.com>
2025-07-29 02:46:18 -0600

    Metodo de escribir archivo

--- src/insertarContenidoArchivo/AgregarTexto.java
+++ src/insertarContenidoArchivo/AgregarTexto.java
@@ -1,7 +1,37 @@
 package insertarContenidoArchivo;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class AgregarTexto {
+    public static void main(String[] args) {
+        boolean anexar = false;
+
+        String nombreArchivo = "mi_archivo.txt";
+
+        File archivo = new File(nombreArchivo);
+
+        try{
+
+        // Revisar si existe el archivo
+
+            anexar = archivo.exists();
+
+            PrintWriter escribir = new PrintWriter(new FileWriter(archivo,anexar));
+            String nuevoContenido = "\nNuevo\ncontenido";
+            escribir.println(nuevoContenido);
+            // Cerramos el flujo abierto
+
+            escribir.close();
 
 
+        }catch (Exception e){
+            System.out.println("Error al escribir en el archivo");
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
